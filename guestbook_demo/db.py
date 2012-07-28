e4ed3a09b271ba00db924391386dc701aa19e084
Eevee <git@veekun.com>
2012-07-28 15:31:51 -0700

    Support heroku db environ variable.

--- guestbook_demo/db.py
+++ guestbook_demo/db.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -6,7 +7,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.schema import Column
 from sqlalchemy.types import DateTime, Integer, Unicode
 
-engine = create_engine('postgresql:///guestbook_demo')
+engine = create_engine(os.environ.get('HEROKU_POSTGRESQL_JADE_URL', 'postgresql:///guestbook_demo'))
 session = scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 Base = declarative_base(bind=engine)
