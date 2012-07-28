9aca69520bb5bd3dba18221ca2f2dab4161fa122
Eevee <git@veekun.com>
2012-07-28 13:37:59 -0700

    Add some templates.

--- guestbook_demo/app.py
+++ guestbook_demo/app.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask('guestbook_demo')
 
 
 @app.route("/")
 def root():
-    return "Wow this is totally useless so far!"
+    return render_template('index.html')
