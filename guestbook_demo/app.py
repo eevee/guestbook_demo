92f112dc3701ed5bd68a68c48a3a50b91694a113
Eevee <git@veekun.com>
2012-07-28 14:33:11 -0700

    Database stuff.

--- guestbook_demo/app.py
+++ guestbook_demo/app.py
@@ -1,7 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 
+from guestbook_demo import db
+
 from flask import Flask, render_template
 app = Flask('guestbook_demo')
+
+
+@app.teardown_request
+def shutdown_session(exception=None):
+    db.session.remove()
 
 
 @app.route("/")
---
+++ guestbook_demo/db.py
@@ -0,0 +1,28 @@
+import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.schema import Column
+from sqlalchemy.types import DateTime, Integer, Unicode
+
+engine = create_engine('postgresql:///guestbook_demo')
+session = scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+Base = declarative_base(bind=engine)
+
+
+### Yonder tables
+
+class GuestbookEntry(Base):
+    __tablename__ = 'guestbook_entries'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    timestamp = Column(DateTime, nullable=False, index=True)
+    name = Column(Unicode, nullable=False)
+    message = Column(Unicode, nullable=False)
+
+    def __init__(self, **kwargs):
+        kwargs.setdefault('timestamp', datetime.utcnow())
+
+        super(GuestbookEntry, self).__init__(**kwargs)
