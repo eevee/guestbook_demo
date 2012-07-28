b6e7bdacf92b9aa8a62671b24c367c136807e93b
Eevee <git@veekun.com>
2012-07-28 15:26:12 -0700

    AAAAND bind to everything.

--- guestbook_demo/__main__.py
+++ guestbook_demo/__main__.py
@@ -3,7 +3,9 @@ import os
 
 from guestbook_demo.app import app
 
+host = None
 port = None
 if 'PORT' in os.environ:
+    host = '0.0.0.0'
     port = int(os.environ['PORT'])
-app.run(port=port)
+app.run(host=host, port=port)
