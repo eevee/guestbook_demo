1b7c80fbd236923c17c14b0ace7bb3e741ca5ee1
Eevee <git@veekun.com>
2012-07-28 14:59:48 -0700

    Make it possible to sign the guestbook.

--- guestbook_demo/app.py
+++ guestbook_demo/app.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from guestbook_demo import db
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 app = Flask('guestbook_demo')
 
 
@@ -18,3 +18,14 @@ def root():
         .order_by(db.GuestbookEntry.timestamp.desc())
 
     return render_template('index.html', entries=entries)
+
+@app.route("/sign", methods=['POST'])
+def signme():
+    new_entry = db.GuestbookEntry(
+        name=request.form.get('name') or 'Some dummy who forgot to leave a name',
+        message=request.form.get('message') or 'WOW THIS IS THE BEST WEBSITE EVER',
+    )
+    db.session.add(new_entry)
+    db.session.commit()
+
+    return redirect(url_for('root'))
